423cbf200b3c4c1f4dbb2eac7d1c8f8fa0fa199c
HellLord <ratul.debnath.year@gmail.com>
2025-01-09 18:24:39 +0600

    implement single worker synchronization

--- src/server/apps/game.py
+++ src/server/apps/game.py
@@ -1,4 +1,5 @@
-import os
+import functools
+from asyncio import Lock
 
 from fastapi import FastAPI
 from fastapi import HTTPException
@@ -8,24 +9,34 @@ from fastapi.responses import FileResponse
 from fastapi.responses import PlainTextResponse
 from fastapi.staticfiles import StaticFiles
 from httpx import AsyncClient
+from httpx import HTTPStatusError
+from httpx import URL
 
 from .. import config
 
 app = FastAPI()
 
-app.mount("/game", StaticFiles(directory=os.path.join(config.DATA_DIR, "game")))
+app.mount("/game", StaticFiles(directory=config.DATA_DIR / "game"))
+
+not_found_lock = functools.lru_cache(lambda _: Lock())
 
 
 @app.exception_handler(status.HTTP_404_NOT_FOUND)
 async def not_found(request: Request, _: HTTPException):
-    path = request.url.path.removeprefix("/game")
-    async with AsyncClient() as client:
-        response = await client.get(f"https://game.doaxvv.com{path}")
-        if response.status_code == status.HTTP_200_OK:
-            path_ = os.path.join(config.DATA_DIR, "game", path.removeprefix("/"))
-            os.makedirs(os.path.dirname(path_), exist_ok=True)
-            with open(path_, "wb") as file:
-                file.write(response.content)
-            return FileResponse(path_)
-        else:
-            return PlainTextResponse("Not found\n", status.HTTP_404_NOT_FOUND)
+    url_path = request.url.path.removeprefix("/game")
+    path = config.DATA_DIR / "game" / url_path.removeprefix("/")
+    async with not_found_lock(url_path):
+        if not path.is_file():
+            async with AsyncClient() as client:
+                url = URL(url_path).copy_with(scheme="https", host="game.doaxvv.com")
+                try:
+                    response = await client.get(url)
+                    response.raise_for_status()
+                except HTTPStatusError:
+                    return PlainTextResponse("Not found\n", status.HTTP_404_NOT_FOUND)
+
+                path.parent.mkdir(parents=True, exist_ok=True)
+                temp_path = path.with_suffix(".tmp")
+                temp_path.write_bytes(response.content)
+                temp_path.rename(path)
+        return FileResponse(path)
