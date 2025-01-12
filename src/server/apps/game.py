c983dae30d5a8f5509ea9e5b6a46fa008a441e60
HellLord <ratul.debnath.year@gmail.com>
2025-01-12 21:18:24 +0600

    sub-app exceptions dont follow hierarchy

--- src/server/apps/game.py
+++ src/server/apps/game.py
@@ -1,7 +1,7 @@
 import functools
+import shutil
 from asyncio import Lock
 
-from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi import Request
 from fastapi import status
@@ -14,32 +14,38 @@ from httpx import HTTPStatusError
 from .. import config
 from .. import consts
 
-app = FastAPI()
-
-app.mount("/game", StaticFiles(directory=consts.GAME_DIR))
-
-get_async_client = functools.cache(
-    lambda: AsyncClient(base_url=consts.GAME_URL, trust_env=not config.NO_PROXY)
-)
-get_path_lock = functools.lru_cache(lambda _: Lock())
+app = StaticFiles(directory=consts.GAME_DIR)
 
 
-@app.exception_handler(status.HTTP_404_NOT_FOUND)
+@functools.lru_cache
+def get_path_lock(_: str):
+    return Lock()
+
+
+@functools.cache
+def get_client():
+    return AsyncClient(base_url=consts.GAME_URL, trust_env=not config.NO_PROXY)
+
+
 async def not_found(request: Request, _: HTTPException):
     url_path = request.url.path.removeprefix("/game")
     path = consts.GAME_DIR / url_path.removeprefix("/")
     async with get_path_lock(url_path):
-        if not path.is_file():
+        if not await path.is_file():
+            client = get_client()
             try:
-                response = await get_async_client().get(url_path)
+                response = await client.get(url_path)
                 response.raise_for_status()
             except HTTPStatusError:
                 return PlainTextResponse(
                     "Not Found", status_code=status.HTTP_404_NOT_FOUND
                 )
             else:
-                path.parent.mkdir(parents=True, exist_ok=True)
+                if await path.is_dir():
+                    shutil.rmtree(path)
+
+                await path.parent.mkdir(parents=True, exist_ok=True)
                 temp_path = path.with_suffix(".tmp")
-                temp_path.write_bytes(response.content)
+                await temp_path.write_bytes(response.content)
                 temp_path.rename(path)
     return FileResponse(path)
