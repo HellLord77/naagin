c96e7259ad4c6f075e0b6285eabb790935c1e8d4
HellLord <ratul.debnath.year@gmail.com>
2025-01-17 02:16:00 +0600

    simplify boolean enums

--- src/naagin/schemas/option_item_auto_lock.py
+++ src/naagin/schemas/option_item_auto_lock.py
@@ -3,8 +3,8 @@ from sqlalchemy import Integer
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
-from naagin.types.enums import OptionLockEnum
-from naagin.types.enums.schemas import OptionLockEnumSchema
+from naagin.types.enums import BooleanEnum
+from naagin.types.enums.schemas import BooleanEnumSchema
 from .base import BaseSchema
 from .owner import OwnerSchema
 
@@ -15,12 +15,12 @@ class OptionItemAutoLockSchema(BaseSchema):
     owner_id: Mapped[int] = mapped_column(
         Integer, ForeignKey(OwnerSchema.owner_id), primary_key=True
     )
-    option_lock_only: Mapped[OptionLockEnum] = mapped_column(
-        OptionLockEnumSchema, default=OptionLockEnum.LOCK
+    option_lock_only: Mapped[BooleanEnum] = mapped_column(
+        BooleanEnumSchema, default=BooleanEnum.TRUE
     )
-    option_lock_sr: Mapped[OptionLockEnum] = mapped_column(
-        OptionLockEnumSchema, default=OptionLockEnum.LOCK
+    option_lock_sr: Mapped[BooleanEnum] = mapped_column(
+        BooleanEnumSchema, default=BooleanEnum.TRUE
     )
-    option_lock_ssr: Mapped[OptionLockEnum] = mapped_column(
-        OptionLockEnumSchema, default=OptionLockEnum.LOCK
+    option_lock_ssr: Mapped[BooleanEnum] = mapped_column(
+        BooleanEnumSchema, default=BooleanEnum.TRUE
     )
--- src/naagin/types/enums/option_lock.py
+++
@@ -1,6 +0,0 @@
-from enum import IntEnum
-
-
-class OptionLockEnum(IntEnum):
-    UNLOCK = 0
-    LOCK = 1
