6f74475c2e6ed1242fbba92f4f41ea8892485678
HellLord <ratul.debnath.year@gmail.com>
2025-01-16 14:20:45 +0600

    fix datetime serializer

--- src/server/models/base.py
+++ src/server/models/base.py
@@ -1,6 +1,11 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 from pydantic import ConfigDict
 
 
 class NaaginBaseModel(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(
+        from_attributes=True,
+        json_encoders={datetime: lambda value: value.strftime("%Y-%m-%d %H:%M:%S")},
+    )
