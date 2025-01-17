6632d727acca175cf6b1597a4317963635bc00d0
HellLord <ratul.debnath.year@gmail.com>
2025-01-17 17:09:59 +0600

    GET /honor

--- src/naagin/schemas/__init__.py
+++ src/naagin/schemas/__init__.py
@@ -1,5 +1,6 @@
 from .episode import EpisodeSchema
 from .girl import GirlSchema
+from .honor import HonorSchema
 from .option_item_auto_lock import OptionItemAutoLockSchema
 from .owner import OwnerSchema
 from .tutorial import TutorialSchema
