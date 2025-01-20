b102bbef899f0116437a7fc22f14fd86e1f7dafe
HellLord <ratul.debnath.year@gmail.com>
2025-01-20 12:53:58 +0600

    add ItemEquipmentSchema

---
+++ src/naagin/schemas/item_equipemnt.py
@@ -0,0 +1,44 @@
+from sqlalchemy import CheckConstraint
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+from naagin.types.enums import BooleanEnum
+from naagin.types.enums import ItemEquipmentTypeEnum
+from naagin.types.enums.schemas import BooleanEnumSchema
+from naagin.types.enums.schemas import ItemEquipmentTypeEnumSchema
+from .base import BaseSchema
+from .owner import OwnerSchema
+
+
+class ItemEquipmentSchema(BaseSchema):
+    __tablename__ = "item_equipment"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    item_mid: Mapped[int] = mapped_column(Integer)
+    type: Mapped[ItemEquipmentTypeEnum] = mapped_column(ItemEquipmentTypeEnumSchema)
+    level: Mapped[int] = mapped_column(Integer, default=1)
+    experience: Mapped[int] = mapped_column(Integer, default=0)
+    girl_mid: Mapped[int] = mapped_column(Integer, default=0)
+    favorite: Mapped[BooleanEnum] = mapped_column(
+        BooleanEnumSchema, default=BooleanEnum.FALSE
+    )
+    in_lock: Mapped[BooleanEnum] = mapped_column(
+        BooleanEnumSchema, default=BooleanEnum.FALSE
+    )
+    unlock_count: Mapped[int] = mapped_column(Integer, default=0)
+    upgrade_count: Mapped[int] = mapped_column(Integer, default=0)
+    combine_count: Mapped[int] = mapped_column(Integer, default=0)
+
+    owner_id: Mapped[int] = mapped_column(Integer, ForeignKey(OwnerSchema.owner_id))
+
+    __table_args__ = (
+        CheckConstraint(id >= 1, "id_min"),
+        CheckConstraint(type != ItemEquipmentTypeEnum.OTHER, "type_const"),
+        CheckConstraint(level.between(1, 90), "level_range"),
+        CheckConstraint(experience.between(0, 480000), "experience_range"),
+        CheckConstraint(unlock_count.between(0, 4), "unlock_count_range"),
+        CheckConstraint(upgrade_count >= 0, "upgrade_count_min"),
+        CheckConstraint(combine_count.between(0, 4), "combine_count_range"),
+    )
