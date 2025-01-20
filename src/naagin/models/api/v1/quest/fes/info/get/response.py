4d921cfa0f73d937e963e69a30dda073e342ee7f
HellLord <ratul.debnath.year@gmail.com>
2025-01-20 17:05:08 +0600

    GET /quest/fes/info

---
+++ src/naagin/models/api/v1/quest/fes/info/get/response.py
@@ -0,0 +1,6 @@
+from naagin.models.base import BaseModel
+
+
+class QuestFesInfoGetResponseModel(BaseModel):
+    open_bonus_fes_list: list
+    quest_daily_info_list: list
---
+++ src/naagin/routers/api/v1/quest/fes/info.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+from naagin.models.api import QuestFesInfoGetResponseModel
+
+router = APIRouter(prefix="/info")
+
+
+@router.get("")
+async def get() -> QuestFesInfoGetResponseModel:
+    return QuestFesInfoGetResponseModel(
+        open_bonus_fes_list=[], quest_daily_info_list=[]
+    )
