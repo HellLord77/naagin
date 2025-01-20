9559703fbdbf4539e8c681d9643cd4c81315b3b6
HellLord <ratul.debnath.year@gmail.com>
2025-01-20 12:52:42 +0600

    add more enums

---
+++ src/naagin/types/enums/client_type.py
@@ -0,0 +1,6 @@
+from enum import IntEnum
+
+
+class ClientTypeEnum(IntEnum):
+    STEAM = 1
+    JOHREN = 5
---
+++ src/naagin/types/enums/schemas/__init__.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Enum
+
+from naagin.types.enums import AppealUpEnum
+from naagin.types.enums import BooleanEnum
+from naagin.types.enums import CheckedLicenseLevelEnum
+from naagin.types.enums import FriendshipStateEnum
+from naagin.types.enums import LicenseLevelEnum
+from naagin.types.enums import OwnerStatusEnum
+from naagin.types.enums import ItemEquipmentTypeEnum
+from .utils import values_callable
+from sqlalchemy import Enum
+
+from naagin.types.enums import AppealUpEnum
+from naagin.types.enums import BooleanEnum
+from naagin.types.enums import CheckedLicenseLevelEnum
+from naagin.types.enums import FriendshipStateEnum
+from naagin.types.enums import ItemEquipmentTypeEnum
+from naagin.types.enums import LicenseLevelEnum
+from naagin.types.enums import OwnerStatusEnum
+from .utils import values_callable
+
+AppealUpEnumSchema = Enum(AppealUpEnum, values_callable=values_callable)
+BooleanEnumSchema = Enum(BooleanEnum, values_callable=values_callable)
+CheckedLicenseLevelEnumSchema = Enum(
+    CheckedLicenseLevelEnum, values_callable=values_callable
+)
+FriendshipStateEnumSchema = Enum(FriendshipStateEnum, values_callable=values_callable)
+ItemEquipmentTypeEnumSchema = Enum(
+    ItemEquipmentTypeEnum, values_callable=values_callable
+)
+LicenseLevelEnumSchema = Enum(LicenseLevelEnum, values_callable=values_callable)
+OwnerStatusEnumSchema = Enum(OwnerStatusEnum, values_callable=values_callable)
