6e245c092b33a1445a1f1c6a413190ec1fc15cd0
HellLord <ratul.debnath.year@gmail.com>
2025-01-21 23:18:15 +0600

    read csv using correct encoding

--- src/naagin/providers/csv/__init__.py
+++ src/naagin/providers/csv/__init__.py
@@ -1,56 +1,42 @@
-from csv import DictReader
-from csv import QUOTE_ALL
-from csv import reader
 from itertools import islice
 
-from naagin import settings
 from naagin.models.csv import EpisodeCSVModel
 from naagin.models.csv import GirlCSVModel
 from naagin.models.csv import GirlStatusCSVModel
 from naagin.types.headers import MasterVersionHeader
+from .utils import get_dict_reader
+from .utils import get_reader
 
 
 async def provide_girl_affection_levels(
     master_version: MasterVersionHeader,
 ) -> list[int]:
-    csv_data = await (
-        settings.data.csv_dir / str(master_version) / "Girl_Affection_Level.csv"
-    ).read_text(errors="ignore")
-    csv_reader = reader(csv_data.splitlines(), quoting=QUOTE_ALL)
-    girl_affection_levels = next(islice(csv_reader, 2, None))
+    reader = await get_reader(master_version, "Girl_Affection_Level")
+    girl_affection_levels = next(islice(reader, 2, None))
     del girl_affection_levels[:2]
     count = int(girl_affection_levels.pop(0)) + 1
     return list(map(int, islice(girl_affection_levels, None, count)))
 
 
 async def provide_girl_levels(master_version: MasterVersionHeader) -> list[int]:
-    csv_data = await (
-        settings.data.csv_dir / str(master_version) / "girl_level_table.csv"
-    ).read_text(errors="ignore")
-    csv_reader = reader(csv_data.splitlines(), quoting=QUOTE_ALL)
-    girl_levels = next(islice(csv_reader, 2, None))
+    reader = await get_reader(master_version, "girl_level_table")
+    girl_levels = next(islice(reader, 2, None))
     del girl_levels[:2]
     count = int(girl_levels.pop(0)) + 1
     return list(map(int, islice(girl_levels, None, count)))
 
 
 async def provide_girls(master_version: MasterVersionHeader) -> dict[int, GirlCSVModel]:
-    csv_data = await (
-        settings.data.csv_dir / str(master_version) / "girl_master.csv"
-    ).read_text()
-    dict_reader = DictReader(csv_data.splitlines(), quoting=QUOTE_ALL)
-    girls = map(GirlCSVModel.model_validate, dict_reader)
+    reader = await get_dict_reader(master_version, "girl_master")
+    girls = map(GirlCSVModel.model_validate, reader)
     return {girl.girl_mid: girl for girl in girls}
 
 
 async def provide_girl_statuses(
     master_version: MasterVersionHeader,
 ) -> list[GirlStatusCSVModel]:
-    csv_data = await (
-        settings.data.csv_dir / str(master_version) / "girl_status_table.csv"
-    ).read_text(errors="ignore")
-    csv_reader = reader(csv_data.splitlines(), quoting=QUOTE_ALL)
-    girl_statuses = islice(csv_reader, 3, None)
+    reader = await get_reader(master_version, "girl_status_table")
+    girl_statuses = islice(reader, 3, None)
     appeals = next(girl_statuses)
     del appeals[:3]
     powers_or_technics = next(girl_statuses)
@@ -69,11 +55,8 @@ async def provide_girl_statuses(
 
 
 async def provide_owner_levels(master_version: MasterVersionHeader) -> list[int]:
-    csv_data = await (
-        settings.data.csv_dir / str(master_version) / "owner_level_table.csv"
-    ).read_text()
-    csv_reader = reader(csv_data.splitlines(), quoting=QUOTE_ALL)
-    owner_levels = next(csv_reader)
+    reader = await get_reader(master_version, "owner_level_table")
+    owner_levels = next(reader)
     count = int(owner_levels.pop(0))
     return list(map(int, islice(owner_levels, None, count)))
 
@@ -81,9 +64,6 @@ async def provide_owner_levels(master_version: MasterVersionHeader) -> list[int]
 async def provide_episodes(
     master_version: MasterVersionHeader,
 ) -> dict[int, EpisodeCSVModel]:
-    csv_data = await (
-        settings.data.csv_dir / str(master_version) / "EpisodeList.csv"
-    ).read_text()
-    dict_reader = DictReader(csv_data.splitlines(), quoting=QUOTE_ALL)
-    episodes = map(EpisodeCSVModel.model_validate, dict_reader)
+    reader = await get_dict_reader(master_version, "EpisodeList")
+    episodes = map(EpisodeCSVModel.model_validate, reader)
     return {episode.episode_mid: episode for episode in episodes}
---
+++ src/naagin/providers/csv/utils.py
@@ -0,0 +1,22 @@
+from csv import DictReader
+from csv import QUOTE_ALL
+from csv import reader
+
+from naagin import settings
+
+
+async def get_lines(master_version: int, name: str) -> list[str]:
+    data = await (
+        settings.data.csv_dir / str(master_version) / f"{name}.csv"
+    ).read_text("shift_jis")
+    return data.splitlines()
+
+
+async def get_reader(master_version: int, name: str) -> reader:
+    lines = await get_lines(master_version, name)
+    return reader(lines, quoting=QUOTE_ALL)
+
+
+async def get_dict_reader(master_version: int, name: str) -> DictReader:
+    lines = await get_lines(master_version, name)
+    return DictReader(lines, quoting=QUOTE_ALL)
