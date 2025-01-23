c70504073d6f5579e28b218d16968899e4721c1a
HellLord <ratul.debnath.year@gmail.com>
2025-01-24 01:44:31 +0600

    correct header optionality

--- src/naagin/types/headers/__init__.py
+++ src/naagin/types/headers/__init__.py
@@ -1,13 +1,26 @@
 from typing import Annotated
+from typing import Optional
 
 from fastapi import Header
 
 from naagin.enums import ClientTypeEnum
+from naagin.enums import EncodingEnum
 from .utils import default_factory
 
-AccessTokenHeader = Annotated[
-    str, Header(default_factory=default_factory, alias="X-DOAXVV-Access-Token")
+ContentTypeHeader = Annotated[str, Header(alias="Content-Type")]
+ContentLengthHeader = Annotated[
+    Optional[int], Header(default_factory=default_factory, alias="Content-Length")
 ]
+
+AccessTokenHeader = Annotated[str, Header(alias="X-DOAXVV-Access-Token")]
+NonceHeader = Annotated[str, Header(alias="X-DOAXVV-Nonce")]
 ApplicationVersionHeader = Annotated[int, Header(alias="X-DOAXVV-ApplicationVersion")]
 ClientTypeHeader = Annotated[ClientTypeEnum, Header(alias="X-DOAXVV-ClientType")]
+EncodingHeader = Annotated[
+    Optional[EncodingEnum],
+    Header(default_factory=default_factory, alias="X-DOAXVV-Encoding"),
+]
+EncryptedHeader = Annotated[
+    Optional[str], Header(default_factory=default_factory, alias="X-DOAXVV-Encrypted")
+]
 MasterVersionHeader = Annotated[int, Header(alias="X-DOAXVV-MasterVersion")]
