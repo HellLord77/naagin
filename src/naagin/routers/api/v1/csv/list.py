0492fed2ca5d6d420c9564e95ca2c80b7db85a08
HellLord <ratul.debnath.year@gmail.com>
2025-01-24 01:46:32 +0600

    apparently, csv also changes between patches

--- src/naagin/routers/api/v1/csv/list.py
+++ src/naagin/routers/api/v1/csv/list.py
@@ -10,5 +10,11 @@ router = APIRouter(prefix="/list")
 @router.get("")
 @alru_cache
 async def get() -> CsvListGetResponseModel:
-    json_data = await (settings.data.api_dir / "v1" / "csv" / "list.json").read_bytes()
+    json_data = await (
+        settings.data.api_dir
+        / "v1"
+        / "csv"
+        / "list"
+        / f"{settings.version.application}.json"
+    ).read_bytes()
     return CsvListGetResponseModel.model_validate_json(json_data)
