81f8ab53e669ecf0544a84e5cba6e6d00b1fcde9
HellLord <ratul.debnath.year@gmail.com>
2025-01-30 22:15:18 +0600

    separate middleware

---
+++ src/naagin/__init__.py
@@ -0,0 +1,9 @@
+__author__ = "HellLord77"
+__copyright__ = ""
+__credits__ = ["HellLord77"]
+
+__license__ = ""
+__version__ = "0.0.1"
+__maintainer__ = "HellLord77"
+__email__ = "ratul.debnath.year@gmail.com"
+__status__ = "Development"
