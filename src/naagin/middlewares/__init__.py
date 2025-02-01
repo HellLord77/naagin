a31945e6b176a96a606f90baf201893931ff1ac8
HellLord <ratul.debnath.year@gmail.com>
2025-02-01 21:26:24 +0600

    misc

--- src/naagin/middlewares/__init__.py
+++ src/naagin/middlewares/__init__.py
@@ -9,11 +9,11 @@ from naagin.utils import should_endec
 async def encode_response_body_middleware(request: Request, call_next):
     response = await call_next(request)
     if should_endec(request.scope):
-        if hasattr(response, "body_iterator"):
-            if settings.api.compress:
-                response_compress_body(response)
-            if settings.api.encrypt:
-                response_encrypt_body(response, request.state.session_key)
-        else:
+        if not hasattr(response, "body_iterator"):
             raise NotImplementedError
+
+        if settings.api.compress:
+            response_compress_body(response)
+        if settings.api.encrypt:
+            response_encrypt_body(response, request.state.session_key)
     return response
