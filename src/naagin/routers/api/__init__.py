b149a8c3e6ba7036e0f18787e1aed3760e01b985
HellLord <ratul.debnath.year@gmail.com>
2025-02-02 11:55:57 +0600

    revert back to injector for response middleware

--- src/naagin/routers/api/__init__.py
+++ src/naagin/routers/api/__init__.py
@@ -2,12 +2,15 @@ from fastapi import APIRouter
 from fastapi import Depends
 
 from naagin import injectors
+from naagin import settings
 from . import v1
 
-router = APIRouter(
-    dependencies=[
-        Depends(injectors.response.inject_doaxvv_headers),
-    ]
-)
+dependencies = [
+    Depends(injectors.response.inject_doaxvv_headers),
+]
+if settings.api.encrypt:
+    dependencies.append(Depends(injectors.request.inject_session_key))
+
+router = APIRouter(dependencies=dependencies)
 
 router.include_router(v1.router)
