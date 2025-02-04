95f2508d36c9a0c2096836a624307f95c7351e55
HellLord <ratul.debnath.year@gmail.com>
2025-02-05 00:52:15 +0600

    simplify exception handling

--- src/naagin/middlewares/__init__.py
+++ src/naagin/middlewares/__init__.py
@@ -1,7 +1,6 @@
 from fastapi import Request
 
 from naagin.exceptions.base import BaseException
-from naagin.utils.exception_handlers import base_exception_handler
 from . import request
 from . import response
 
@@ -10,6 +9,6 @@ async def handle_base_exception(request: Request, call_next):
     try:
         response = await call_next(request)
     except BaseException as exception:
-        return await base_exception_handler(request, exception)
+        return exception.handle()
     else:
         return response
--- src/naagin/utils/exception_handlers.py
+++ src/naagin/utils/exception_handlers.py
@@ -6,13 +6,7 @@ from fastapi import Response
 from fastapi.responses import JSONResponse
 
 from naagin import apps
-from naagin.exceptions import InternalServerErrorException
-from naagin.exceptions import InvalidParameterException
-from naagin.exceptions import MethodNotAllowedException
 from naagin.exceptions import NotFoundException
-from naagin.exceptions.base import BaseException
-from naagin.models.common import ExceptionModel
-from . import DOAXVVHeader
 
 
 async def moved_permanently_handler(_: Request, __: HTTPException) -> JSONResponse:
@@ -26,31 +20,4 @@ async def not_found_handler(request: Request, exception: HTTPException) -> Respo
     if request.url.path.startswith("/game"):
         return await apps.game.not_found_handler(request, exception)
     else:
-        return await base_exception_handler(request, NotFoundException())
-
-
-async def method_not_allowed_handler(
-    request: Request, _: HTTPException
-) -> JSONResponse:
-    return await base_exception_handler(request, MethodNotAllowedException())
-
-
-async def unprocessable_content_handler(
-    request: Request, _: HTTPException
-) -> JSONResponse:
-    return await base_exception_handler(request, InvalidParameterException())
-
-
-async def internal_server_error_handler(
-    request: Request, _: HTTPException
-) -> JSONResponse:
-    return await base_exception_handler(request, InternalServerErrorException())
-
-
-async def base_exception_handler(_: Request, exception: BaseException) -> JSONResponse:
-    response = JSONResponse(
-        ExceptionModel.model_validate(exception).model_dump(),
-        exception.code if exception.code in HTTPStatus else HTTPStatus.OK,
-    )
-    DOAXVVHeader.set(response, "Status", exception.code)
-    return response
+        return NotFoundException.handle()
