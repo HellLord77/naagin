d8dff9778dd5c41e19ec156433521b5781bbbab9
HellLord <ratul.debnath.year@gmail.com>
2025-02-07 01:03:44 +0600

    [PrivateItemSchema] BooleanEnum -> bool

--- src/naagin/models/api/__init__.py
+++ src/naagin/models/api/__init__.py
@@ -1,16 +1,32 @@
 from .v1.bromide.get.response import BromideGetResponseModel as BromideGetResponseModel
-from .v1.cheat_log.check.get.response import CheatLogCheckGetResponseModel as CheatLogCheckGetResponseModel
+from .v1.cheat_log.check.get.response import (
+    CheatLogCheckGetResponseModel as CheatLogCheckGetResponseModel,
+)
 from .v1.csv.list.get.response import CsvListGetResponseModel as CsvListGetResponseModel
 from .v1.friendship.__friend_id__.delete.response import (
     FriendshipFriendIdDeleteResponseModel as FriendshipFriendIdDeleteResponseModel,
 )
-from .v1.friendship.accept.post.request import FriendshipAcceptPostRequestModel as FriendshipAcceptPostRequestModel
-from .v1.friendship.accept.post.response import FriendshipAcceptPostResponseModel as FriendshipAcceptPostResponseModel
-from .v1.friendship.get.response import FriendshipGetResponseModel as FriendshipGetResponseModel
-from .v1.friendship.post.request import FriendshipPostRequestModel as FriendshipPostRequestModel
-from .v1.friendship.post.response import FriendshipPostResponseModel as FriendshipPostResponseModel
-from .v1.friendship.received.get.response import FriendshipReceivedGetResponseModel as FriendshipReceivedGetResponseModel
-from .v1.friendship.sent.get.response import FriendshipSentGetResponseModel as FriendshipSentGetResponseModel
+from .v1.friendship.accept.post.request import (
+    FriendshipAcceptPostRequestModel as FriendshipAcceptPostRequestModel,
+)
+from .v1.friendship.accept.post.response import (
+    FriendshipAcceptPostResponseModel as FriendshipAcceptPostResponseModel,
+)
+from .v1.friendship.get.response import (
+    FriendshipGetResponseModel as FriendshipGetResponseModel,
+)
+from .v1.friendship.post.request import (
+    FriendshipPostRequestModel as FriendshipPostRequestModel,
+)
+from .v1.friendship.post.response import (
+    FriendshipPostResponseModel as FriendshipPostResponseModel,
+)
+from .v1.friendship.received.get.response import (
+    FriendshipReceivedGetResponseModel as FriendshipReceivedGetResponseModel,
+)
+from .v1.friendship.sent.get.response import (
+    FriendshipSentGetResponseModel as FriendshipSentGetResponseModel,
+)
 from .v1.girl.__girl_mid__.private.favorite.__type__.get.response import (
     GirlGirlMidPrivateFavoriteTypeGetResponseModel as GirlGirlMidPrivateFavoriteTypeGetResponseModel,
 )
@@ -20,24 +36,44 @@ from .v1.girl.__girl_mid__.private.favorite.__type__.post.request import (
 from .v1.girl.__girl_mid__.private.favorite.__type__.post.response import (
     GirlGirlMidPrivateFavoriteTypePostResponseModel as GirlGirlMidPrivateFavoriteTypePostResponseModel,
 )
-from .v1.girl.equipment.get.response import GirlEquipmentGetResponseModel as GirlEquipmentGetResponseModel
+from .v1.girl.equipment.get.response import (
+    GirlEquipmentGetResponseModel as GirlEquipmentGetResponseModel,
+)
 from .v1.girl.get.response import GirlGetResponseModel as GirlGetResponseModel
-from .v1.girl.potential.get.response import GirlPotentialGetResponseModel as GirlPotentialGetResponseModel
+from .v1.girl.potential.get.response import (
+    GirlPotentialGetResponseModel as GirlPotentialGetResponseModel,
+)
 from .v1.girl.private.favorite.__type__.get.response import (
     GirlPrivateFavoriteTypeGetResponseModel as GirlPrivateFavoriteTypeGetResponseModel,
 )
-from .v1.girl.private.get.response import GirlPrivateGetResponseModel as GirlPrivateGetResponseModel
+from .v1.girl.private.get.response import (
+    GirlPrivateGetResponseModel as GirlPrivateGetResponseModel,
+)
 from .v1.honor.get.response import HonorGetResponseModel as HonorGetResponseModel
-from .v1.item.consume.get.response import ItemConsumeGetResponseModel as ItemConsumeGetResponseModel
-from .v1.item.consume.negative.get.response import ItemConsumeNegativeGetResponseModel as ItemConsumeNegativeGetResponseModel
+from .v1.item.consume.get.response import (
+    ItemConsumeGetResponseModel as ItemConsumeGetResponseModel,
+)
+from .v1.item.consume.negative.get.response import (
+    ItemConsumeNegativeGetResponseModel as ItemConsumeNegativeGetResponseModel,
+)
 from .v1.item.equipment.type.__type__.get.response import (
     ItemEquipmentTypeTypeGetResponseModel as ItemEquipmentTypeTypeGetResponseModel,
 )
-from .v1.option.item_auto_lock.post.request import OptionItemAutoLockPostRequestModel as OptionItemAutoLockPostRequestModel
-from .v1.option.item_auto_lock.post.response import OptionItemAutoLockPostResponseModel as OptionItemAutoLockPostResponseModel
-from .v1.owner.birthday.post.request import OwnerBirthdayPostRequestModel as OwnerBirthdayPostRequestModel
-from .v1.owner.birthday.post.response import OwnerBirthdayPostResponseModel as OwnerBirthdayPostResponseModel
-from .v1.owner.countlogin.get.response import OwnerCountloginGetResponseModel as OwnerCountloginGetResponseModel
+from .v1.option.item_auto_lock.post.request import (
+    OptionItemAutoLockPostRequestModel as OptionItemAutoLockPostRequestModel,
+)
+from .v1.option.item_auto_lock.post.response import (
+    OptionItemAutoLockPostResponseModel as OptionItemAutoLockPostResponseModel,
+)
+from .v1.owner.birthday.post.request import (
+    OwnerBirthdayPostRequestModel as OwnerBirthdayPostRequestModel,
+)
+from .v1.owner.birthday.post.response import (
+    OwnerBirthdayPostResponseModel as OwnerBirthdayPostResponseModel,
+)
+from .v1.owner.countlogin.get.response import (
+    OwnerCountloginGetResponseModel as OwnerCountloginGetResponseModel,
+)
 from .v1.owner.episode.__episode_mid__.post.response import (
     OwnerEpisodeEpisodeMidPostResponseModel as OwnerEpisodeEpisodeMidPostResponseModel,
 )
@@ -53,25 +89,47 @@ from .v1.owner.put.response import OwnerPutResponseModel as OwnerPutResponseMode
 from .v1.pvp_fes_deck.equipment_list_all.get.response import (
     PvpFesDeckEquipmentListAllGetResponseModel as PvpFesDeckEquipmentListAllGetResponseModel,
 )
-from .v1.pvp_girl.equipment.get.response import PvpGirlEquipmentGetResponseModel as PvpGirlEquipmentGetResponseModel
+from .v1.pvp_girl.equipment.get.response import (
+    PvpGirlEquipmentGetResponseModel as PvpGirlEquipmentGetResponseModel,
+)
 from .v1.quest.check.license_point.post.request import (
     QuestCheckLicensePointPostRequestModel as QuestCheckLicensePointPostRequestModel,
 )
 from .v1.quest.check.license_point.post.response import (
     QuestCheckLicensePointPostResponseModel as QuestCheckLicensePointPostResponseModel,
 )
-from .v1.quest.fes.info.get.response import QuestFesInfoGetResponseModel as QuestFesInfoGetResponseModel
-from .v1.quest.stamina.get.response import QuestStaminaGetResponseModel as QuestStaminaGetResponseModel
-from .v1.session.key.get.response import SessionKeyGetResponseModel as SessionKeyGetResponseModel
-from .v1.session.key.put.request import SessionKeyPutRequestModel as SessionKeyPutRequestModel
-from .v1.session.key.put.response import SessionKeyPutResponseModel as SessionKeyPutResponseModel
+from .v1.quest.fes.info.get.response import (
+    QuestFesInfoGetResponseModel as QuestFesInfoGetResponseModel,
+)
+from .v1.quest.stamina.get.response import (
+    QuestStaminaGetResponseModel as QuestStaminaGetResponseModel,
+)
+from .v1.session.key.get.response import (
+    SessionKeyGetResponseModel as SessionKeyGetResponseModel,
+)
+from .v1.session.key.put.request import (
+    SessionKeyPutRequestModel as SessionKeyPutRequestModel,
+)
+from .v1.session.key.put.response import (
+    SessionKeyPutResponseModel as SessionKeyPutResponseModel,
+)
 from .v1.session.post.request import SessionPostRequestModel as SessionPostRequestModel
-from .v1.session.post.response import SessionPostResponseModel as SessionPostResponseModel
+from .v1.session.post.response import (
+    SessionPostResponseModel as SessionPostResponseModel,
+)
 from .v1.shop.paymentlog.incomplete.get.response import (
     ShopPaymentlogIncompleteGetResponseModel as ShopPaymentlogIncompleteGetResponseModel,
 )
-from .v1.special_order.__type__.get.resposne import SpecialOrderTypeGetResponseModel as SpecialOrderTypeGetResponseModel
-from .v1.tutorial.__event_mid__.put.request import TutorialEventMidPutRequestModel as TutorialEventMidPutRequestModel
-from .v1.tutorial.__event_mid__.put.response import TutorialEventMidPutResponseModel as TutorialEventMidPutResponseModel
-from .v1.tutorial.get.response import TutorialGetResponseModel as TutorialGetResponseModel
+from .v1.special_order.__type__.get.resposne import (
+    SpecialOrderTypeGetResponseModel as SpecialOrderTypeGetResponseModel,
+)
+from .v1.tutorial.__event_mid__.put.request import (
+    TutorialEventMidPutRequestModel as TutorialEventMidPutRequestModel,
+)
+from .v1.tutorial.__event_mid__.put.response import (
+    TutorialEventMidPutResponseModel as TutorialEventMidPutResponseModel,
+)
+from .v1.tutorial.get.response import (
+    TutorialGetResponseModel as TutorialGetResponseModel,
+)
 from .v1.wallet.get.response import WalletGetResponseModel as WalletGetResponseModel
