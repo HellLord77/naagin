d859a062a26786c93a822356708972c63f25d790
HellLord <ratul.debnath.year@gmail.com>
2025-02-07 18:01:04 +0600

    GET owner/episode

--- src/naagin/routers/api/v1/owner/episode/__init__.py
+++ src/naagin/routers/api/v1/owner/episode/__init__.py
@@ -1,7 +1,19 @@
 from fastapi import APIRouter
+from sqlalchemy import select
+
+from naagin.models.api import OwnerEpisodeGetResponseModel
+from naagin.schemas import EpisodeSchema
+from naagin.types.dependencies import OwnerIdDependency
+from naagin.types.dependencies import SessionDependency
 
 from . import __episode_mid__
 
 router = APIRouter(prefix="/episode")
 
 router.include_router(__episode_mid__.router)
+
+
+@router.get("")
+async def get(session: SessionDependency, owner_id: OwnerIdDependency) -> OwnerEpisodeGetResponseModel:
+    episode_list = (await session.scalars(select(EpisodeSchema).where(EpisodeSchema.owner_id == owner_id))).all()
+    return OwnerEpisodeGetResponseModel(episode_list=episode_list)
