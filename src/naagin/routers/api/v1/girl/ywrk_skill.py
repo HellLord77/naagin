aff6ed518c56147b38386119d6cd07c945e5b547
HellLord <ratul.debnath.year@gmail.com>
2025-02-07 17:43:54 +0600

    GET girl/ywrk_skill

--- src/naagin/routers/api/v1/girl/ywrk_skill.py
+++ src/naagin/routers/api/v1/girl/ywrk_skill.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter
+from sqlalchemy import select
 
 from naagin.models.api import GirlYwrkSkillGetResponseModel
+from naagin.schemas import YwrkSkillSchema
+from naagin.types.dependencies import OwnerIdDependency
+from naagin.types.dependencies import SessionDependency
 
 router = APIRouter(prefix="/ywrk_skill")
 
 
 @router.get("")
-async def get() -> GirlYwrkSkillGetResponseModel:
-    raise NotImplementedError
+async def get(session: SessionDependency, owner_id: OwnerIdDependency) -> GirlYwrkSkillGetResponseModel:
+    ywrk_skill_list = (await session.scalars(select(YwrkSkillSchema).where(YwrkSkillSchema.owner_id == owner_id))).all()
+    return GirlYwrkSkillGetResponseModel(ywrk_skill_list=ywrk_skill_list)
