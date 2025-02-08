999dfd2e7db8256771a42e35bfb9bc90fab31a64
HellLord <ratul.debnath.year@gmail.com>
2025-02-08 15:43:35 +0600

    dishevelment -> dishevelment_swimsuit

--- src/naagin/routers/api/v1/dishevelment/__owner_id__/__item_mid__.py
+++ src/naagin/routers/api/v1/dishevelment/__owner_id__/__item_mid__.py
@@ -3,7 +3,7 @@ from sqlalchemy import select
 
 from naagin.models.api import DishevelmentOwnerIdItemMidGetResponseModel
 from naagin.models.api.v1.dishevelment.__owner_id__.__item_mid__.get.response import DishevelmentOtherModel
-from naagin.schemas import DishevelmentSchema
+from naagin.schemas import DishevelmentSwimsuitSchema
 from naagin.types.dependencies import SessionDependency
 
 router = APIRouter(prefix="/{item_mid}")
@@ -11,16 +11,16 @@ router = APIRouter(prefix="/{item_mid}")
 
 @router.get("")
 async def get(owner_id: int, item_mid: int, session: SessionDependency) -> DishevelmentOwnerIdItemMidGetResponseModel:
-    dishevelment = await session.scalar(
-        select(DishevelmentSchema).where(
-            DishevelmentSchema.owner_id == owner_id, DishevelmentSchema.item_mid == item_mid
+    dishevelment_swimsuit = await session.scalar(
+        select(DishevelmentSwimsuitSchema).where(
+            DishevelmentSwimsuitSchema.owner_id == owner_id, DishevelmentSwimsuitSchema.item_mid == item_mid
         )
     )
 
     variation = 1
-    dishevelment_ = dishevelment is not None
+    dishevelment = dishevelment_swimsuit is not None
 
     dishevelment_other = DishevelmentOtherModel(
-        owner_id=owner_id, item_mid=item_mid, variation=variation, dishevelment=dishevelment_
+        owner_id=owner_id, item_mid=item_mid, variation=variation, dishevelment=dishevelment
     )
     return DishevelmentOwnerIdItemMidGetResponseModel(dishevelment_other=dishevelment_other)
