df3d8237f1dafc39b66a6d5eeb5ddcf7a145853f
HellLord <ratul.debnath.year@gmail.com>
2025-02-10 17:12:23 +0600

    use literal in schema

--- src/naagin/schemas/dishevelment_swimsuit.py
+++ src/naagin/schemas/dishevelment_swimsuit.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from sqlalchemy import CheckConstraint
 from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
@@ -13,6 +15,6 @@ class DishevelmentSwimsuitSchema(BaseSchema):
 
     owner_id: Mapped[int] = mapped_column(Integer, ForeignKey(OwnerSchema.owner_id), primary_key=True)
     item_mid: Mapped[int] = mapped_column(Integer, primary_key=True)
-    variation: Mapped[int] = mapped_column(Integer, default=1)
+    variation: Mapped[Literal[1]] = mapped_column(Integer, default=1)
 
     __table_args__ = (CheckConstraint(variation == 1, "variation_const"),)
