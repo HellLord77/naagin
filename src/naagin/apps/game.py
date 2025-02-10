cb3ee79dea99b882a2e780638951351c6cb2d9cb
HellLord <ratul.debnath.year@gmail.com>
2025-02-10 14:43:54 +0600

    simplify httpx client

--- src/naagin/apps/game.py
+++ src/naagin/apps/game.py
@@ -1,5 +1,4 @@
 from asyncio import Lock
-from functools import cache
 from functools import lru_cache
 from http import HTTPStatus
 from shutil import rmtree
@@ -10,7 +9,6 @@ from fastapi import Response
 from fastapi.responses import FileResponse
 from fastapi.responses import PlainTextResponse
 from fastapi.staticfiles import StaticFiles
-from httpx import AsyncClient
 from httpx import HTTPStatusError
 
 from naagin import settings
@@ -27,11 +25,6 @@ def get_path_lock(_: str) -> Lock:
     return Lock()
 
 
-@cache
-def get_client() -> AsyncClient:
-    return AsyncClient(base_url=settings.game.base_url, trust_env=not settings.game.no_proxy)
-
-
 async def not_found_handler(request: Request, _: HTTPException) -> Response:
     if settings.game.offline_mode:
         return not_found_response()
@@ -41,7 +34,7 @@ async def not_found_handler(request: Request, _: HTTPException) -> Response:
         async with get_path_lock(url_path):
             if not await path.is_file():
                 try:
-                    response = await get_client().get(url_path)
+                    response = await settings.game.client.get(url_path)
                     response.raise_for_status()
                 except HTTPStatusError:
                     return not_found_response()
--- src/naagin/routers/api01/v1/johren/authJohren/__onetime_token__.py
+++ src/naagin/routers/api01/v1/johren/authJohren/__onetime_token__.py
@@ -1,7 +1,4 @@
-from functools import cache
-
 from fastapi import APIRouter
-from httpx import AsyncClient
 
 from naagin import settings
 from naagin.models.api01 import JohrenAuthJohrenOnetimeTokenGetResponse
@@ -9,14 +6,9 @@ from naagin.models.api01 import JohrenAuthJohrenOnetimeTokenGetResponse
 router = APIRouter(prefix="/{onetime_token}")
 
 
-@cache
-def get_client() -> AsyncClient:
-    return AsyncClient(base_url=settings.api01.base_url, trust_env=not settings.api01.no_proxy)
-
-
 @router.get("")
 async def get(onetime_token: str) -> JohrenAuthJohrenOnetimeTokenGetResponse:
-    response = await get_client().get(f"/v1/johren/authJohren/{onetime_token}")
+    response = await settings.api01.client.get(f"/v1/johren/authJohren/{onetime_token}")
     response.raise_for_status()
 
     json_data = response.content
