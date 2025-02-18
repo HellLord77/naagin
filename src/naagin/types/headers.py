8529bb0fd421338aca1ab04a247d825ab138cbe6
HellLord <ratul.debnath.year@gmail.com>
2025-02-19 01:32:40 +0600

    remove factories.common

--- src/naagin/types/headers.py
+++ src/naagin/types/headers.py
@@ -8,21 +8,14 @@ from naagin.enums import ClientTypeEnum
 from naagin.enums import EncodingEnum
 
 ContentTypeHeader = Annotated[str, Header(alias="Content-Type")]
-ContentLengthHeader = Annotated[
-    int | None, Header(default_factory=factories.common.null_factory, alias="Content-Length")
-]
+ContentLengthHeader = Annotated[int | None, Header(default_factory=factories.null_factory, alias="Content-Length")]
 
-AccessTokenHeader = Annotated[
-    str | None, Header(default_factory=factories.common.null_factory, alias="X-DOAXVV-Access-Token")
-]
+AccessTokenHeader = Annotated[str | None, Header(default_factory=factories.null_factory, alias="X-DOAXVV-Access-Token")]
 NonceHeader = Annotated[str, Header(alias="X-DOAXVV-Nonce")]
 ApplicationVersionHeader = Annotated[int, Header(alias="X-DOAXVV-ApplicationVersion")]
 ClientTypeHeader = Annotated[ClientTypeEnum, Header(alias="X-DOAXVV-ClientType")]
 EncodingHeader = Annotated[
-    Literal[EncodingEnum.DEFLATE] | None,
-    Header(default_factory=factories.common.null_factory, alias="X-DOAXVV-Encoding"),
+    Literal[EncodingEnum.DEFLATE] | None, Header(default_factory=factories.null_factory, alias="X-DOAXVV-Encoding")
 ]
-EncryptedHeader = Annotated[
-    str | None, Header(default_factory=factories.common.null_factory, alias="X-DOAXVV-Encrypted")
-]
+EncryptedHeader = Annotated[str | None, Header(default_factory=factories.null_factory, alias="X-DOAXVV-Encrypted")]
 MasterVersionHeader = Annotated[int, Header(alias="X-DOAXVV-MasterVersion")]
