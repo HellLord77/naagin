d2223e2c8cea64257b0b0fb4281102c475a9bf99
HellLord <ratul.debnath.year@gmail.com>
2025-02-19 01:33:26 +0600

    add FriendlyRewardCSVModel

--- src/dumper/flows.py
+++ src/dumper/flows.py
@@ -138,13 +138,22 @@ def to_model():
     rmtree_empty(get_json_dir())
 
     shutil.rmtree(get_schema_dir(), True)
-    json_to_schema = functools.partial(utils.json_to_schema, json_dir=get_json_dir(), schema_dir=get_schema_dir())
-    json_dirs = set(map(operator.attrgetter("parent"), filter(Path.is_file, get_json_dir().rglob("*.json"))))
+    json_to_schema = functools.partial(
+        utils.json_to_schema, json_dir=get_json_dir(), schema_dir=get_schema_dir()
+    )
+    json_dirs = set(
+        map(
+            operator.attrgetter("parent"),
+            filter(Path.is_file, get_json_dir().rglob("*.json")),
+        )
+    )
     with ThreadPoolExecutor() as executor:
         executor.map(json_to_schema, json_dirs)
 
     shutil.rmtree(get_model_dir(), True)
-    schema_to_model = functools.partial(utils.schema_to_model, schema_dir=get_schema_dir(), model_dir=get_model_dir())
+    schema_to_model = functools.partial(
+        utils.schema_to_model, schema_dir=get_schema_dir(), model_dir=get_model_dir()
+    )
     schema_paths = filter(Path.is_file, get_schema_dir().rglob("*.schema.json"))
     with ProcessPoolExecutor() as executor:
         executor.map(schema_to_model, schema_paths)
---
+++ src/naagin/models/csv/friendly_reward.py
@@ -0,0 +1,18 @@
+from naagin.bases import ModelBase
+
+
+class FriendlyRewardCSVModel(ModelBase):
+    _column_1: int
+    friendly: int
+    level: int
+    _column_4: int
+    basic_item_mid: int
+    basic_item_count: int
+    _column_7: None
+    _column_8: None
+    _column_9: None
+    _column_10: int
+    _column_11: int
+    _column_12: int
+    _column_13: int
+    photo_spot_mid: int
