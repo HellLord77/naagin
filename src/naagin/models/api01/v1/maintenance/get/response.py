36b83c11fe9710f1b67e334737b43777b44bdcdf
HellLord <ratul.debnath.year@gmail.com>
2025-02-19 12:26:44 +0600

    GET /api01/v1/maintenance
    add maintenance data

--- src/naagin/models/api01/v1/maintenance/get/response.py
+++ src/naagin/models/api01/v1/maintenance/get/response.py
@@ -3,3 +3,4 @@ from naagin.bases import ModelBase
 
 class MaintenanceGetResponseModel(ModelBase):
     maintenance: bool
+    maintenance_datetime: str | None = None
--- src/naagin/routers/api01/v1/maintenance/__init__.py
+++ src/naagin/routers/api01/v1/maintenance/__init__.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
+from sqlalchemy import func
 
 from naagin.models.api01 import MaintenanceGetResponseModel
+from naagin.schemas import MaintenanceSchema
+from naagin.types.dependencies import SessionDependency
 
 from . import privilege
 
@@ -10,5 +13,13 @@ router.include_router(privilege.router)
 
 
 @router.get("")
-async def get() -> MaintenanceGetResponseModel:
-    return MaintenanceGetResponseModel(maintenance=False)
+async def get(session: SessionDependency) -> MaintenanceGetResponseModel:
+    maintenance = await session.find(
+        MaintenanceSchema, func.current_timestamp().between(MaintenanceSchema.started_at, MaintenanceSchema.end_at)
+    )
+
+    if maintenance is None:
+        return MaintenanceGetResponseModel(maintenance=False)
+    started_at = maintenance.started_at.strftime("%Y/%m/%d %H:%M:%S")
+    end_at = maintenance.started_at.strftime("%Y/%m/%d %H:%M:%S")
+    return MaintenanceGetResponseModel(maintenance=True, maintenance_datetime=f"{started_at} ~ {end_at}")
