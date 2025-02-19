c1aafe39d53bf95767ba9b9aad526c705b0ea2ed
HellLord <ratul.debnath.year@gmail.com>
2025-02-19 14:28:11 +0600

    update version

--- src/naagin/settings/version.py
+++ src/naagin/settings/version.py
@@ -8,7 +8,7 @@ from naagin.bases import SettingsBase
 
 class VersionSettings(SettingsBase):
     master: Literal[10] = 10
-    application: int = 64501
+    application: int = 64700
 
     model_config = SettingsConfigDict(env_prefix="ver_")
 
