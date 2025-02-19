2d7b528827d7d5d77677b1f70615e21669db49ca
HellLord <ratul.debnath.year@gmail.com>
2025-02-19 15:48:44 +0600

    simplify middleware stack

--- src/naagin/dependencies/__init__.py
+++ src/naagin/dependencies/__init__.py
@@ -4,7 +4,14 @@ from time import time
 from fastapi import Response
 
 from naagin import settings
+from naagin.exceptions import UnderMaintenanceNowException
+from naagin.types.dependencies import MaintenanceDependency
 from naagin.utils import CustomHeader
+
+
+def check_maintenance(maintenance: MaintenanceDependency) -> None:
+    if maintenance is not None:
+        raise UnderMaintenanceNowException
 
 
 def add_custom_headers(response: Response) -> None:
