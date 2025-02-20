e23c6b3fbb3bcc0d9f8fb4ca123a8e19a6c75874
HellLord <ratul.debnath.year@gmail.com>
2025-02-20 12:36:57 +0600

    POST room/request/cancel

---
+++ src/naagin/routers/api/v1/room/request/cancel.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+
+from naagin.models.api import RoomRequestCancelPostResponseModel
+from naagin.schemas import RequestSchema
+from naagin.types.dependencies import DatabaseDependency
+from naagin.types.dependencies import OwnerIdDependency
+
+router = APIRouter(prefix="/cancel")
+
+
+@router.post("")
+async def post(database: DatabaseDependency, owner_id: OwnerIdDependency) -> RoomRequestCancelPostResponseModel:
+    request = await database.get_one(RequestSchema, owner_id)
+
+    request.request_mid = 0
+    request.girl_mid1 = 0
+    request.girl_mid2 = 0
+    request.trend_status = False
+    request.started_at = None
+    request.end_at = None
+
+    await database.flush()
+    await database.refresh(request)
+
+    return RoomRequestCancelPostResponseModel(custom_room_request_list=[request])
