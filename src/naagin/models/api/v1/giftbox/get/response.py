b0082a6731fb0a0ad0dc3e6d214735a913ac48b8
HellLord <ratul.debnath.year@gmail.com>
2025-02-28 21:37:43 +0600

    merge giftbox models

--- src/naagin/models/api/v1/giftbox/get/response.py
+++ src/naagin/models/api/v1/giftbox/get/response.py
@@ -1,20 +1,5 @@
-from datetime import datetime
-
 from naagin.bases import ModelBase
-
-
-class GiftBoxModel(ModelBase):
-    id: int
-    sender_id: int
-    sender_name: str
-    item_mid: int
-    count: int
-    message_type: int
-    message: str
-    parameter1: int
-    created_at: datetime
-    expired_at: datetime
-    accepted_at: None
+from naagin.models import GiftBoxModel
 
 
 class GiftBoxGetResponseModel(ModelBase):
