4049cc7209566049a892ccecadec2372c262b549
HellLord <ratul.debnath.year@gmail.com>
2025-02-28 21:37:43 +0600

    merge giftbox models

--- src/naagin/models/__init__.py
+++ src/naagin/models/__init__.py
@@ -5,6 +5,7 @@ from .favorite_private_item import FavoritePrivateItemModel as FavoritePrivateIt
 from .friendly_value import FriendlyValueModel as FriendlyValueModel
 from .friendship import FriendshipModel as FriendshipModel
 from .furniture_my_set import FurnitureMySetModel as FurnitureMySetModel
+from .giftbox_model import GiftBoxModel as GiftBoxModel
 from .girl_equipment_model import GirlEquipmentModel as GirlEquipmentModel
 from .item_consume import ItemConsumeModel as ItemConsumeModel
 from .owner import OwnerModel as OwnerModel
--- src/naagin/models/api/v1/giftbox/fetch/post/response.py
+++ src/naagin/models/api/v1/giftbox/fetch/post/response.py
@@ -1,23 +1,8 @@
-from datetime import datetime
-
 from naagin.bases import ModelBase
+from naagin.models import GiftBoxModel
 from naagin.models import OwnerCheckedAtModel
 
 
-class GiftBoxFetchModel(ModelBase):
-    id: int
-    sender_id: int
-    sender_name: str
-    item_mid: int
-    count: int
-    message_type: int
-    message: str
-    parameter1: int
-    created_at: datetime
-    expired_at: datetime
-    accepted_at: None
-
-
 class GiftBoxFetchPostResponseModel(ModelBase):
-    giftbox_fetch_list: list[GiftBoxFetchModel]
+    giftbox_fetch_list: list[GiftBoxModel]
     owner_checked_at_list: list[OwnerCheckedAtModel] | None = None
