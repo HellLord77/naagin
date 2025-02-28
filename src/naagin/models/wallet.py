077364dffba80d9b5afb93ccc1b0e53b3aafe862
HellLord <ratul.debnath.year@gmail.com>
2025-02-28 23:04:36 +0600

    merge wallet models

---
+++ src/naagin/models/wallet.py
@@ -0,0 +1,11 @@
+from naagin.bases import ModelBase
+
+
+class WalletModel(ModelBase):
+    owner_id: int
+    zack_money: int
+    guest_point: int
+    vip_point: int
+    paid_vstone: int
+    free_vstone: int
+    vip_coin: int
