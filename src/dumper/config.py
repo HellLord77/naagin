c91e2496d979dd9bfbaa7d39351b9a3d713eaa49
HellLord <ratul.debnath.year@gmail.com>
2025-03-07 21:44:42 +0600

    update version

--- src/dumper/config.py
+++ src/dumper/config.py
@@ -4,6 +4,6 @@ from pathlib import Path
 EXAMPLES: bool = os.getenv("EXAMPLES", "false").lower() == "true"
 
 VERSION: str = os.getenv("VERSION", 10)
-APP_VERSION: int = int(os.getenv("APP_VERSION", 64700))
+APP_VERSION: int = int(os.getenv("APP_VERSION", 64800))
 
 DATA_DIR: Path = Path(os.getenv("DATA_DIR", Path.cwd() / "data"))
