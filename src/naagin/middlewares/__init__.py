360da18b4ca40f1966278745f825de260966a23c
HellLord <ratul.debnath.year@gmail.com>
2025-03-10 11:04:04 +0600

    remove redirect

--- src/naagin/middlewares/__init__.py
+++ src/naagin/middlewares/__init__.py
@@ -3,5 +3,4 @@ from .deflate import DeflateMiddleware as DeflateMiddleware
 from .middleware.filtered import FilteredMiddleware as FilteredMiddleware
 from .middleware.renewed import RenewedMiddleware as RenewedMiddleware
 from .middleware.stacked import StackedMiddleware as StackedMiddleware
-from .redirect import RedirectMiddleware as RedirectMiddleware
 from .request.limiting_body import LimitingBodyRequestMiddleware as LimitingBodyRequestMiddleware
