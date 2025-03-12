d7db4d5b5ff0deea90920910526c2c0675d5ad57
HellLord <ratul.debnath.year@gmail.com>
2025-03-12 16:16:31 +0600

    load dll from system directory

--- tools/winhttp/hook.h
+++ tools/winhttp/hook.h
@@ -7,14 +7,34 @@
 
 FAKE(BOOL, WINAPI, WinHttpGetIEProxyConfigForCurrentUser, WINHTTP_CURRENT_USER_IE_PROXY_CONFIG *pProxyConfig)
 {
-    if (!pProxyConfig) {
+    if (!pProxyConfig)
+    {
         SetLastError(ERROR_INVALID_PARAMETER);
         return FALSE;
     }
 
     LPCWSTR proxy = L"http://localhost:8080";
     SIZE_T len = wcslen(proxy) + 1;
-    pProxyConfig->lpszProxy = (LPWSTR)GlobalAlloc(GPTR, len * sizeof(WCHAR));
+    pProxyConfig->lpszProxy = (LPWSTR)GlobalAlloc(GMEM_FIXED, len * sizeof(WCHAR));
+    if (!pProxyConfig->lpszProxy)
+    {
+        return FALSE;
+    }
+
     wcscpy_s(pProxyConfig->lpszProxy, len, proxy);
+    SetLastError(ERROR_SUCCESS);
     return TRUE;
 }
+
+#undef LoadLibrary
+#define LoadLibrary(lpLibFileName) \
+    ([]() -> HMODULE { \
+        CHAR lpBuffer[MAX_PATH]; \
+        if (!GetSystemDirectory(lpBuffer, MAX_PATH)) { \
+            return NULL; \
+        } \
+        \
+        CHAR lpLibFilePath[MAX_PATH]; \
+        sprintf_s(lpLibFilePath, MAX_PATH, "%s\\%s", lpBuffer, lpLibFileName + 5); \
+        return LoadLibraryA(lpLibFilePath); \
+    })()
