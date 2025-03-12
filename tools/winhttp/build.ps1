06554660236f559e89f54e08b7de99fe6b6d8a2d
HellLord <ratul.debnath.year@gmail.com>
2025-03-12 16:16:31 +0600

    load dll from system directory

--- tools/winhttp/build.ps1
+++ tools/winhttp/build.ps1
@@ -1,6 +1,2 @@
-$content = Get-Content -Path "winhttp.cpp" -Raw
-$content = $content.Replace("real_winhttp.dll", "C:\\Windows\\System32\\winhttp.dll")
-$content | Set-Content -Path "winhttp.cpp"
-
 cmake -B build
 cmake --build build --config=Release
