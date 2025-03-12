63a3dcfe0c40e00532ad56e00fed4f38a2184eea
HellLord <ratul.debnath.year@gmail.com>
2025-03-13 02:12:39 +0600

    bypass proxy

--- hooks/winhttp.h
+++ hooks/winhttp.h
@@ -2,6 +2,72 @@
 
 #include "common.h"
 
+#ifndef _DEBUG
+#include <wchar.h>
+
+#define WINHTTPCRACKURL
+FAKE(BOOL, WINAPI, WinHttpCrackUrl, LPCWSTR pwszUrl, DWORD dwUrlLength, DWORD dwFlags, LPURL_COMPONENTS lpUrlComponents)
+{
+    if (lpUrlComponents->dwHostNameLength < 9)
+    {
+        SetLastError(ERROR_NOT_ENOUGH_MEMORY);
+        return FALSE;
+    }
+
+    if (!lpUrlComponents->dwUrlPathLength)
+    {
+        SetLastError(ERROR_INVALID_PARAMETER);
+        return FALSE;
+    }
+    DWORD lpszUrlPathLengthMax = lpUrlComponents->dwUrlPathLength;
+
+    if (WinHttpCrackUrl_real(pwszUrl, dwUrlLength, dwFlags, lpUrlComponents))
+    {
+        LPWSTR lpszHostName;
+        DWORD dwHostNameLength;
+        if (lpUrlComponents->dwHostNameLength == 14 && !wmemcmp(lpUrlComponents->lpszHostName, L"api.doaxvv.com", 14))
+        {
+            lpszHostName = L"/api";
+            dwHostNameLength = 4;
+        }
+        else if (lpUrlComponents->dwHostNameLength == 15 && !wmemcmp(lpUrlComponents->lpszHostName, L"game.doaxvv.com", 15))
+        {
+            lpszHostName = L"/game";
+            dwHostNameLength = 5;
+        }
+        else if (lpUrlComponents->dwHostNameLength == 16 && !wmemcmp(lpUrlComponents->lpszHostName, L"api01.doaxvv.com", 16))
+        {
+            lpszHostName = L"/api01";
+            dwHostNameLength = 6;
+        }
+        else
+        {
+            SetLastError(ERROR_WINHTTP_INVALID_URL);
+            return FALSE;
+        }
+
+        lpUrlComponents->nScheme = INTERNET_SCHEME_HTTP;
+        lpUrlComponents->lpszHostName = L"localhost";
+        lpUrlComponents->dwHostNameLength = 9;
+        lpUrlComponents->nPort = 8000;
+
+        if (wmemmove_s(lpUrlComponents->lpszUrlPath + dwHostNameLength, lpszUrlPathLengthMax - dwHostNameLength, lpUrlComponents->lpszUrlPath, lpUrlComponents->dwUrlPathLength))
+        {
+            SetLastError(ERROR_NOT_ENOUGH_MEMORY);
+            return FALSE;
+        }
+        if (wmemcpy_s(lpUrlComponents->lpszUrlPath, lpszUrlPathLengthMax, lpszHostName, dwHostNameLength))
+        {
+            SetLastError(ERROR_WINHTTP_INTERNAL_ERROR);
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+
+    return FALSE;
+}
+#else
 #define WINHTTPOPEN
 FAKE(HINTERNET, WINAPI, WinHttpOpen, LPCWSTR pszAgentW, DWORD dwAccessType, LPCWSTR pszProxyW, LPCWSTR pszProxyBypassW, DWORD dwFlags)
 {
@@ -9,3 +75,4 @@ FAKE(HINTERNET, WINAPI, WinHttpOpen, LPCWSTR pszAgentW, DWORD dwAccessType, LPCW
     pszProxyW = L"http://localhost:8080";
     return WinHttpOpen_real(pszAgentW, dwAccessType, pszProxyW, pszProxyBypassW, dwFlags);
 }
+#endif
