b98a7c741290511f2579dfcf445397f9b74546fd
HellLord <ratul.debnath.year@gmail.com>
2025-03-13 21:27:28 +0600

    dll for seamless redirection

--- hooks/common.h
+++ hooks/common.h
@@ -1,11 +1,12 @@
 #pragma once
 
 #include <windows.h>
+#include <wchar.h>
 
 #include "hook_macro.h"
 
 #undef LoadLibrary
-inline HMODULE LoadLibrary(LPCSTR lpLibFileName)
+HMODULE LoadLibrary(LPCSTR lpLibFileName)
 {
     CHAR lpBuffer[MAX_PATH];
     if (!GetSystemDirectoryA(lpBuffer, MAX_PATH))
@@ -16,4 +17,70 @@ inline HMODULE LoadLibrary(LPCSTR lpLibFileName)
     CHAR lpLibFilePath[MAX_PATH];
     sprintf_s(lpLibFilePath, MAX_PATH, "%s\\%s", lpBuffer, lpLibFileName + 5);
     return LoadLibraryA(lpLibFilePath);
+}
+
+#define SCHEME INTERNET_SCHEME_HTTP
+#define HOST_NAME L"localhost"
+#define HOST_NAME_LENGTH 9
+#define PORT 8000
+
+typedef BOOL(WINAPI *CrackUrl_ptr)(LPCWSTR, DWORD, DWORD, LPURL_COMPONENTSW);
+
+BOOL CrackUrl_fake(CrackUrl_ptr crackUrl_real, LPCWSTR url, DWORD urlLength, DWORD flags, LPURL_COMPONENTSW urlComponents)
+{
+    if (urlComponents->dwHostNameLength <= HOST_NAME_LENGTH)
+    {
+        return FALSE;
+    }
+
+    if (!urlComponents->dwUrlPathLength)
+    {
+        return FALSE;
+    }
+    DWORD urlPathLength = urlComponents->dwUrlPathLength;
+
+    if (crackUrl_real(url, urlLength, flags, urlComponents))
+    {
+        LPWSTR hostName;
+        DWORD hostNameLength;
+
+        if (urlComponents->dwHostNameLength == 14 && !wmemcmp(urlComponents->lpszHostName, L"api.doaxvv.com", 14))
+        {
+            hostName = L"/api";
+            hostNameLength = 4;
+        }
+        else if (urlComponents->dwHostNameLength == 15 && !wmemcmp(urlComponents->lpszHostName, L"game.doaxvv.com", 15))
+        {
+            hostName = L"/game";
+            hostNameLength = 5;
+        }
+        else if (urlComponents->dwHostNameLength == 16 && !wmemcmp(urlComponents->lpszHostName, L"api01.doaxvv.com", 16))
+        {
+            hostName = L"/api01";
+            hostNameLength = 6;
+        }
+        else
+        {
+            return FALSE;
+        }
+
+        urlComponents->nScheme = INTERNET_SCHEME_HTTP;
+        urlComponents->lpszHostName = HOST_NAME;
+        urlComponents->dwHostNameLength = HOST_NAME_LENGTH;
+        urlComponents->nPort = 8000;
+
+        if (wmemmove_s(urlComponents->lpszUrlPath + hostNameLength, urlPathLength - hostNameLength, urlComponents->lpszUrlPath, urlComponents->dwUrlPathLength + 1))
+        {
+            return FALSE;
+        }
+        if (wmemcpy_s(urlComponents->lpszUrlPath, urlPathLength, hostName, hostNameLength))
+        {
+            return FALSE;
+        }
+        urlComponents->dwUrlPathLength += hostNameLength;
+
+        return TRUE;
+    }
+
+    return FALSE;
 }
--- hooks/winhttp.h
+++ hooks/winhttp.h
@@ -2,10 +2,8 @@
 
 #include "common.h"
 
-#define WINHTTPOPEN
-FAKE(HINTERNET, WINAPI, WinHttpOpen, LPCWSTR pszAgentW, DWORD dwAccessType, LPCWSTR pszProxyW, LPCWSTR pszProxyBypassW, DWORD dwFlags)
+#define WINHTTPCRACKURL
+FAKE(BOOL, WINAPI, WinHttpCrackUrl, LPCWSTR pwszUrl, DWORD dwUrlLength, DWORD dwFlags, LPURL_COMPONENTS lpUrlComponents)
 {
-    dwAccessType = WINHTTP_ACCESS_TYPE_NAMED_PROXY;
-    pszProxyW = L"http://localhost:8080";
-    return WinHttpOpen_real(pszAgentW, dwAccessType, pszProxyW, pszProxyBypassW, dwFlags);
+    return CrackUrl_fake(WinHttpCrackUrl_real, pwszUrl, dwUrlLength, dwFlags, lpUrlComponents);
 }
