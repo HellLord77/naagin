70f4ab790752c51e835f07093533e06017f4b91c
HellLord <ratul.debnath.year@gmail.com>
2025-03-15 20:43:27 +0600

    close writer

--- tools/proxy/consts.py
+++ tools/proxy/consts.py
@@ -1,3 +1,5 @@
+import atexit
+import os
 import time
 import urllib.parse
 
@@ -17,3 +19,13 @@ if config.WRITE_FILE:
     FLOW_WRITER = FlowWriter(
         (config.DATA_DIR / "flows" / f"DOAXVV-{int(time.time())}.flows").open("wb")
     )
+
+    def _close_file():
+        file = FLOW_WRITER.fo
+        size = file.tell()
+        file.close()
+
+        if size == 0:
+            os.remove(file.name)
+
+    atexit.register(_close_file)
