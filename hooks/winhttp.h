a60390647add9f3316fe98a2757b4d777a547248
HellLord <ratul.debnath.year@gmail.com>
2025-03-17 13:57:44 +0600

    read config.ini

--- hooks/winhttp.h
+++ hooks/winhttp.h
@@ -3,6 +3,7 @@
 #include "common.h"
 
 #define WINHTTPCRACKURL
+
 FAKE(BOOL, WINAPI, WinHttpCrackUrl, LPCWSTR pwszUrl, DWORD dwUrlLength, DWORD dwFlags, LPURL_COMPONENTS lpUrlComponents)
 {
     return CrackUrl_fake(WinHttpCrackUrl_real, pwszUrl, dwUrlLength, dwFlags, lpUrlComponents);
--- hooks/wininet.h
+++ hooks/wininet.h
@@ -3,6 +3,7 @@
 #include "common.h"
 
 #define INTERNETCRACKURLW
+
 FAKE(BOOL, WINAPI, InternetCrackUrlW, LPCWSTR lpszUrl, DWORD dwUrlLength, DWORD dwFlags, LPURL_COMPONENTSW lpUrlComponents)
 {
     return CrackUrl_fake(InternetCrackUrlW_real, lpszUrl, dwUrlLength, dwFlags, lpUrlComponents);
