44a27107507e7de6ce6797c24496ca42530d7c55
HellLord <ratul.debnath.year@gmail.com>
2025-03-26 22:37:05 +0600

    stricter validation for json paths

--- naagin/models/api/v1/csv/list/get/response.py
+++ naagin/models/api/v1/csv/list/get/response.py
@@ -1,12 +1,16 @@
+from typing import Literal
+
 from pydantic import ConfigDict
 
 from naagin.bases import ModelBase
+from naagin.types.fields import CSVField
+from naagin.types.fields import MD5Field
 
 
 class CsvFileModel(ModelBase):
-    __pydantic_extra__: dict[str, str]
+    __pydantic_extra__: dict[CSVField, MD5Field]
 
-    file_encrypt_key: str
+    file_encrypt_key: Literal["9AraWdtpsar4fln2r1TtX0AxiCJcLSqp"]
 
     model_config = ConfigDict(extra="allow")
 
