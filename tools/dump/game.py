311d4e1c0b6fc535710468205e1a8c23dc21b298
HellLord <ratul.debnath.year@gmail.com>
2025-03-28 21:40:48 +0600

    clean up dir

--- tools/dump/game.py
+++ tools/dump/game.py
@@ -3,10 +3,11 @@ import logging
 import shutil
 from pathlib import Path
 
-from httpx import Client
 from httpx import URL
+from httpx import Client
 
 import config
+import utils
 
 
 def get_md5(path: Path) -> str:
@@ -19,22 +20,22 @@ def get_md5(path: Path) -> str:
 
 def to_tmp():
     game_dir = config.DATA_DIR / "game"
-    temp_dir = config.DATA_DIR / "temp"
     client = Client(base_url=URL(scheme="https", host="game.doaxvv.com"))
 
     for path in game_dir.rglob("*"):
-        if path.is_file():
-            logging.warning("[GAME] %s", path)
+        path: Path
+        if not path.is_file():
+            continue
 
-            relative_path = path.relative_to(game_dir)
+        logging.warning("[GAME] %s", path)
+        relative_path = path.relative_to(game_dir)
 
-            response = client.head(relative_path.as_posix())
-            response.raise_for_status()
-            hash_ = response.headers["ETag"][1:33]
+        response = client.head(relative_path.as_posix())
+        response.raise_for_status()
+        hash_ = response.headers["ETag"][1:33]
 
-            if hash_ != get_md5(path):
-                logging.error(path)
+        if hash_ != get_md5(path):
+            logging.error(path)
+            path.unlink()
 
-                temp_path = (temp_dir / relative_path).with_suffix(".temp")
-                temp_path.parent.mkdir(parents=True, exist_ok=True)
-                path.rename(temp_path)
+    utils.rmtree_empty(game_dir)
