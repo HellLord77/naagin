37b6675488ad4891865ef037873cf3eafbd333e5
HellLord <ratul.debnath.year@gmail.com>
2025-03-28 21:35:41 +0600

    clean up

--- naagin/routers/api/v1/csv/list.py
+++ naagin/routers/api/v1/csv/list.py
@@ -2,15 +2,15 @@ from async_lru import alru_cache
 from fastapi import APIRouter
 
 from naagin import settings
-from naagin.models.api import CsvListGetResponseModel
+from naagin.models.api import CSVListGetResponseModel
 
 router = APIRouter(prefix="/list")
 
 
 @router.get("")
 @alru_cache
-async def get() -> CsvListGetResponseModel:
+async def get() -> CSVListGetResponseModel:
     json_data = await (
         settings.data.api_dir / "v1" / "csv" / "list" / f"{settings.version.application}.json"
     ).read_bytes()
-    return CsvListGetResponseModel.model_validate_json(json_data)
+    return CSVListGetResponseModel.model_validate_json(json_data)
